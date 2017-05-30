63cce76d06b1b9248f6f3bd5a99d05ef91bb0a77
Justin Dubs <jtdubs@gmail.com>
2017-05-30 16:07:09 -0400

    tmep

--- SwissArmyHook/InjectionEntryPoint.cs
+++ SwissArmyHook/InjectionEntryPoint.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using EasyHook;
 using System.Runtime.InteropServices;
+using System.Threading;
 
 namespace SwissArmyHook
 {
@@ -38,7 +39,7 @@ namespace SwissArmyHook
             createFileHook.ThreadACL.SetExclusiveACL(new Int32[] { 0 });
             readFileHook.ThreadACL.SetExclusiveACL(new Int32[] { 0 });
             writeFileHook.ThreadACL.SetExclusiveACL(new Int32[] { 0 });
-
+            
             server.ReportMessage("Installed!");
             RemoteHooking.WakeUpProcess();
 
@@ -68,6 +69,10 @@ namespace SwissArmyHook
             LocalHook.Release();
         }
 
+        // GetOverlappedResult https://msdn.microsoft.com/en-us/library/windows/desktop/ms683209(v=vs.85).aspx
+        // CreateIoCompletionPort  https://msdn.microsoft.com/en-us/library/windows/desktop/aa363862(v=vs.85).aspx
+        // GetQueuedCompletionStatus  https://msdn.microsoft.com/en-us/library/windows/desktop/aa364986(v=vs.85).aspx
+
         #region CreateFile
         [UnmanagedFunctionPointer(CallingConvention.StdCall, CharSet = CharSet.Unicode, SetLastError = true)]
         delegate IntPtr CreateFile_Delegate(String filename, UInt32 desiredAccess, UInt32 shareMode, IntPtr securityAttributes, UInt32 creationDisposition, UInt32 flagsAndAttributes, IntPtr templateFile);
@@ -77,23 +82,24 @@ namespace SwissArmyHook
 
         private IntPtr CreateFile_Hook(String filename, UInt32 desiredAccess, UInt32 shareMode, IntPtr securityAttributes, UInt32 creationDisposition, UInt32 flagsAndAttributes, IntPtr templateFile)
         {
-            if (String.IsNullOrEmpty(filename) || ! filename.StartsWith(@"\\.\pipe\", StringComparison.InvariantCultureIgnoreCase))
-                return CreateFileW(filename, desiredAccess, shareMode, securityAttributes, creationDisposition, flagsAndAttributes, templateFile);
-
             var handle = CreateFileW(filename, desiredAccess, shareMode, securityAttributes, creationDisposition, flagsAndAttributes, templateFile);
-
+          
             try
             {
+                if (handle.ToInt32() == -1)
+                    return handle;
+
+                if (String.IsNullOrEmpty(filename))
+                    return handle;
+                
+                if (!filename.StartsWith(@"\\.\pipe\", StringComparison.InvariantCultureIgnoreCase))
+                    return handle;
+                   
                 namedPipeHandles[handle] = filename;
 
                 lock (queue)
                 {
-                    queue.Enqueue(
-                        string.Format("[{0}:{1}]: PIPE \"{2}\" OPENED ({3:X08})",
-                                EasyHook.RemoteHooking.GetCurrentProcessId(),
-                                EasyHook.RemoteHooking.GetCurrentThreadId(),
-                                filename,
-                                handle.ToInt32()));
+                    queue.Enqueue(String.Format("CreateFile(\"{0}\") = {1:X08}", filename, handle.ToInt32()));
                 }
             }
             catch
@@ -113,27 +119,57 @@ namespace SwissArmyHook
 
         private bool ReadFile_Hook(IntPtr hFile, IntPtr lpBuffer, uint nNumberOfBytesToRead, out uint lpNumberOfBytesRead, IntPtr lpOverlapped)
         {
-            bool result = ReadFile(hFile, lpBuffer, nNumberOfBytesToRead, out lpNumberOfBytesRead, lpOverlapped);
+            if (!namedPipeHandles.ContainsKey(hFile))
+                return ReadFile(hFile, lpBuffer, nNumberOfBytesToRead, out lpNumberOfBytesRead, lpOverlapped);
 
-            try
+            bool result = false;
+
+            if (lpOverlapped == IntPtr.Zero)
             {
-                if (!result || !namedPipeHandles.ContainsKey(hFile))
-                    return result;
+                result = ReadFile(hFile, lpBuffer, nNumberOfBytesToRead, out lpNumberOfBytesRead, lpOverlapped);
 
-                /*
-                // byte[] buffer = new byte[lpNumberOfBytesRead];
-                // Marshal.Copy(lpBuffer, buffer, 0, (int)lpNumberOfBytesRead);
-                */
-
-                lock (queue)
+                if (result)
                 {
-                    queue.Enqueue(string.Format("ReadFile({0:X08}, {1:X08}, {2}, {3}, {4:X08})", 0, 0, 0, 0, 0));
+                    byte[] buffer = new byte[lpNumberOfBytesRead];
+                    Marshal.Copy(lpBuffer, buffer, 0, (int)lpNumberOfBytesRead);
+
+                    lock (queue)
+                    {
+                        queue.Enqueue(String.Format("ReadFile({0:X08}) = {1}", hFile.ToInt32(), BitConverter.ToString(buffer)));
+                    }
                 }
             }
-            catch
+            else
             {
-            }
+                result = ReadFile(hFile, lpBuffer, nNumberOfBytesToRead, out lpNumberOfBytesRead, lpOverlapped);
 
+                if (result)
+                {
+                    lock (queue)
+                    {
+                        queue.Enqueue(String.Format("ReadFile({0:X08}) = async success", hFile.ToInt32()));
+                    }
+                }
+                else if (Marshal.GetLastWin32Error() == 997) // ERROR_IO_PENDING
+                {
+                    var overlapped = Marshal.PtrToStructure<NativeOverlapped>(lpOverlapped);
+
+                    lock (queue)
+                    {
+                        queue.Enqueue(String.Format("ReadFile({0:X08}) = async: {1:X08}", hFile.ToInt32(), lpOverlapped.ToInt32()));
+                    }
+                }
+                else
+                {
+                    var overlapped = Marshal.PtrToStructure<NativeOverlapped>(lpOverlapped);
+
+                    lock (queue)
+                    {
+                        queue.Enqueue(String.Format("ReadFile({0:X08}) = async fail: {1}, {2}", hFile.ToInt32(), Marshal.GetLastWin32Error(), overlapped.InternalLow));
+                    }
+                }
+            }
+            
             return result;
         }
         #endregion
@@ -149,34 +185,54 @@ namespace SwissArmyHook
 
         private bool WriteFile_Hook(IntPtr hFile, IntPtr lpBuffer, uint nNumberOfBytesToWrite, out uint lpNumberOfBytesWritten, IntPtr lpOverlapped)
         {
-            bool result = WriteFile(hFile, lpBuffer, nNumberOfBytesToWrite, out lpNumberOfBytesWritten, lpOverlapped);
-               
-            try
-            {
-                if (!namedPipeHandles.ContainsKey(hFile))
-                    return result;
+            if (!namedPipeHandles.ContainsKey(hFile))
+                return WriteFile(hFile, lpBuffer, nNumberOfBytesToWrite, out lpNumberOfBytesWritten, lpOverlapped);
 
-                /*
-                byte[] buffer = new byte[lpNumberOfBytesWritten];
-                Marshal.Copy(lpBuffer, buffer, 0, (int)lpNumberOfBytesWritten);
+            bool result = false;
+
+            if (lpOverlapped == IntPtr.Zero)
+            {
+                result = WriteFile(hFile, lpBuffer, nNumberOfBytesToWrite, out lpNumberOfBytesWritten, lpOverlapped);
+
+                if (result)
+                {
+                    // byte[] buffer = new byte[lpNumberOfBytesWritten];
+                    // Marshal.Copy(lpBuffer, buffer, 0, (int)lpNumberOfBytesWritten);
+
+                    lock (queue)
+                    {
+                        queue.Enqueue(String.Format("WriteFile({0:X08}) = {1}", hFile.ToInt32(), "data")); // BitConverter.ToString(buffer)));
+                    }
+                }
+            }
+            else
+            {
+                result = WriteFile(hFile, lpBuffer, nNumberOfBytesToWrite, out lpNumberOfBytesWritten, lpOverlapped);
+
+                // var overlapped = Marshal.PtrToStructure<NativeOverlapped>(lpOverlapped);
+
+                /*new ManualResetEvent()
+
+                var newOverlapped = new NativeOverlapped()
+                {
+                    OffsetLow = overlapped.OffsetLow,
+                    OffsetHigh = overlapped.OffsetHigh,
+                    EventHandle = CreateEvent(IntPtr.Zero, true, false, null)
+                };*/
+
                 lock (queue)
                 {
-                    queue.Enqueue(
-                        string.Format("[{0}:{1}]: {3:X016} < {4}",
-                                EasyHook.RemoteHooking.GetCurrentProcessId(),
-                                EasyHook.RemoteHooking.GetCurrentThreadId(),
-                                hFile.ToInt64(),
-                                BitConverter.ToString(buffer)));
+                    queue.Enqueue(String.Format("WriteFile({0:X08}) = {1}", hFile.ToInt32(), "overlapped")); // overlapped.ToString()));
                 }
-                */
             }
-            catch
-            {
-            }
+
 
             return result;
         }
         #endregion
+
+        [DllImport("kernel32.dll")]
+        static extern IntPtr CreateEvent(IntPtr lpEventAttributes, bool bManualReset, bool bInitialState, string lpName);
 
         private ServerInterface server = null;
         private Queue<string> queue = new Queue<string>();
