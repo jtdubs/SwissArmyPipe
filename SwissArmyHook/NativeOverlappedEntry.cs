359172c93142784a43be7df02311b502c3c80540
Justin Dubs <jtdubs@gmail.com>
2017-05-31 11:11:23 -0400

    closer

---
+++ SwissArmyHook/NativeOverlappedEntry.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace SwissArmyHook
+{
+    [StructLayout(LayoutKind.Sequential)]
+    public struct NativeOverlappedEntry
+    {
+        public IntPtr lpCompletionKey;
+        public IntPtr lpOverlapped;
+        public IntPtr Internal;
+        public uint dwNumberOfBytesTransferred;
+    }
+}
